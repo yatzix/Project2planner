9e145b3715b85ec617aa148f7c90ba3a898cf8f7
Mariam Araujo <mariam21betancourt@gmail.com>
2023-04-24 12:00:33 -0700

    mongoose connection

--- config/database.js
+++ config/database.js
@@ -1,1 +1,7 @@
-DATABASE_URL=""
+const mongoose = require('mongoose');
+mongoose.connect(process.env.DATABASE_URL);
+
+const db = mongoose.connection
+db.on('connected', function() {
+    console.log(`Connected to MongoDB ${db.name} at ${db.host}:${db.port}`);
+  });
