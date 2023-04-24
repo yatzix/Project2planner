3e177e4bd80d147bc3ed8be0dbd64d43bbebfcfb
yatzix <gutierrezyatzil@gmail.com>
2023-04-24 09:53:51 -0700

    wip: finishes set up basic

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const logger = require('morgan');
+const indexRoutes = require('./routes/index');
+const app = express();
+
+app.set('view engine', 'ejs');
+require('dotenv').config();
+require('./config/database');
+
+app.use(logger('dev'));
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: false }));
+
+app.use('/', indexRoutes);
+
+app.use('*', (req, res) => {
+    res.render('404', {title: '404 - Page Not Found'});
+});
+
+app.listen(3000, () => {
+    console.log('express is listening on port:3000');
+});
