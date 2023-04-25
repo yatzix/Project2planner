b17ba152646d8d666b5e9a399154af19c281935d
yatzix <gutierrezyatzil@gmail.com>
2023-04-25 16:37:06 -0700

    implements todo functionality

--- models/todo.js
+++ models/todo.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const todoSchema = new Schema({
-   name:{type: String, require: true},
+   name:{type: String, required: true},
    created:{type: Date, defualt: function(){
     return new Date(new Date().setFullYear(new Date().getFullYear() +1))    
 }},
@@ -10,4 +10,4 @@ const todoSchema = new Schema({
    goal: {type: Schema.Types.ObjectId, ref: 'Goal'},
 });
 
-module.exports = mongoose.model('Todos', todoSchema)
+module.exports = mongoose.model('Todo', todoSchema)
--- routes/todos.js
+++ routes/todos.js
@@ -3,8 +3,8 @@ const router = express.Router();
 
 const todosController = require('../controllers/todos');
 
-router.get('/flights/:id/todos/new', todosController.new);
+router.get('/goals/:id/todos/new', todosController.new);
 
-router.post('/flights/:id/todos', todosController.create);
+router.post('/goals/:id/todos', todosController.create);
 
 module.exports = router;
