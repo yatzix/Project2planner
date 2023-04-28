0af554cc07a0faf23c34c7b1686e69c572fc85ce
yatzix <gutierrezyatzil@gmail.com>
2023-04-28 07:40:13 -0700

    fixes date format

--- controllers/todos.js
+++ controllers/todos.js
@@ -2,7 +2,9 @@ const Goal = require('../models/goal');
 const Todo = require('../models/todo');
 
 async function newTodo(req, res) {
+    // Find the goal with the specified ID
     const goal = await Goal.findById(req.params.id);
+    // Render the "todos/new" template, passing in the goal and some data for the template
     res.render('todos/new', {
        todo: "Enter To Do Info",
        goal: goal,
@@ -12,9 +14,13 @@ async function newTodo(req, res) {
 
 async function create(req, res) {
     try {
+    // Find the goal with the specified ID
     const goal = await Goal.findById(req.params.id);
+    // Add the goal ID to the request body
     req.body.goal = req.params.id;
+    // Create a new to-do item in the database
     await Todo.create(req.body)
+    // Redirect the user back to the goal's detail page
     res.redirect(`/goals/${goal.id}`);
     } catch(error){
         console.log(error);
@@ -23,10 +29,15 @@ async function create(req, res) {
 }
 async function deleteTodo(req, res) {
     try{
+         // Find the goal with the specified ID
         const goal = await Goal.findById(req.params.id);
+        // Add the goal ID to the request body
         req.body.goal = req.params.id;
+        // Get the ID of the to-do item to be deleted from the URL parameters
         const todoId = req.params.todoId;
+        // Delete the to-do item from the database using the `findByIdAndRemove` method on the `Todo` model
         await Todo.findByIdAndRemove(req.params.todoId);
+        // Redirect the user back to the goal's detail page
         res.redirect(`/goals/${goal.id}`)
         } catch(error){
             console.log(error)
--- models/todo.js
+++ models/todo.js
@@ -6,9 +6,10 @@ const todoSchema = new Schema({
    todo:{type: String, required: true},
    when:{type: Date, default: () => new Date(new Date().getFullYear() + 1),
     get: function(val) {
+        //checks if val is falsy
         if (!val) {
-            return null;
-        }
+            return null; //if falsy return null
+        }//otherwise convert date value to format specified
         return val.toLocaleDateString('en-US', { month: '2-digit', day: '2-digit', year: 'numeric' });
     }},
 status: {
